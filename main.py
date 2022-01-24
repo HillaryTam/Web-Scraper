5014b4093d78b784fea3f8f15ce121ee2862eec2
HillaryTam <86868861+HillaryTam@users.noreply.github.com>
2022-01-24 16:16:31 +0800

    Upload the main.py file

---
+++ main.py
@@ -0,0 +1,34 @@
+import time
+
+from bs4 import BeautifulSoup
+import requests
+
+print("Input some skills that you are not familiar with")
+unfamiliar_skill = input('>')
+print(f'Filtering out {unfamiliar_skill}')
+
+
+def find_job():
+    html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+    for index, job in enumerate(jobs):
+        published_date = job.find('span', class_='sim-posted').span.text
+        if 'few' in published_date:
+            company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+            skills = job.find('span', class_='srp-skills').text.replace(' ', '')
+            link = job.header.h2.a['href']
+            if unfamiliar_skill not in skills:
+                with open(f'posts/{index}.txt', 'w') as f:
+                    f.write(f"Company Name: {company_name.strip()} \n")
+                    f.write(f"Required Skills: {skills.strip()} \n")
+                    f.write(f"More Info: {link} \n")
+                print(f'File saved: {index}')
+
+
+if __name__ == '__main__':
+    while True:
+        find_job()
+        time_wait = 10
+        print(f'Waiting {time_wait} minutes...')
+        time.sleep(time_wait * 60)
